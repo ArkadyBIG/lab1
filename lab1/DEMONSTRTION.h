ca82944e319883cb0ae8538a8533b4ea00a0f246
ArkadyBIG <arkadyzyganov@gmail.com>
2020-03-10 22:55:14 +0200

    Add files via upload

---
+++ lab1/DEMONSTRTION.h
@@ -0,0 +1,194 @@
+#ifndef DEMONSTRTION_H_INCLUDED
+#define DEMONSTRTION_H_INCLUDED
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int T=500;
+void DEMONSTRATION()
+{
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),6);
+    cout<<"ID          NAME    TYPE        AMOUNT      TIME        DATA               EXP. DATE\n";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),3);
+    cout<<"017565      milk    KG          3.0         12.11       25.02 2000         121\n"
+"016673      Milk    PIES        11.0        12.10       12.09 1090         315\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"SHOW ALL PRODUCTS\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"SEARCH\nEDIT\nBENCHMARK MODE\nEXIT";
+Sleep(T);
+system("cls");
+
+
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),6);
+    cout<<"ID          NAME    TYPE        AMOUNT      TIME        DATA               EXP. DATE\n";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),3);
+    cout<<"017565      milk    KG          3.0         12.11       25.02 2000         121\n"
+"016673      Milk    PIES        11.0        12.10       12.09 1090         315\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"SHOW ALL PRODUCTS\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"SEARCH\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"EDIT\nBENCHMARK MODE\nEXIT";
+Sleep(T);
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),6);
+    cout<<"ID          NAME    TYPE        AMOUNT      TIME        DATA               EXP. DATE\n";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),3);
+    cout<<"017565      milk    KG          3.0         12.11       25.02 2000         121\n"
+"016673      Milk    PIES        11.0        12.10       12.09 1090         315\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"SHOW ALL PRODUCTS\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"SEARCH\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"EDIT\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"BENCHMARK MODE\nEXIT";
+Sleep(T);
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"NEW PRODUCT"<<endl;
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"MAKE CHANGES";
+Sleep(T);
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"NEW PRODUCT #"<<endl;
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"MAKE CHANGES\n\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+cout<<"Enter the name of the product:\n";
+Sleep(T);
+cout<<'J';
+Sleep(T);
+cout<<'u';
+Sleep(T);
+cout<<'i';
+
+Sleep(T);
+cout<<'c';
+Sleep(T);
+cout<<'e';
+
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"\nChose the type of the product:\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"KGRAM\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"LITER\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"PIESE\nBAG";
+Sleep(T);
+
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"NEW PRODUCT #"<<endl;
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"MAKE CHANGES\n\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+cout<<"Enter the name of the product:\n";
+
+cout<<'J';
+
+cout<<'u';
+
+cout<<'i';
+
+cout<<'c';
+
+cout<<'e';
+
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"\nChose the type of the product:\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"KGRAM\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"LITER\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"PIESE\nBAG";
+Sleep(T);
+///////////////////////////////
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"NEW PRODUCT #"<<endl;
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"MAKE CHANGES\n\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+cout<<"Enter the name of the product:\n";
+
+cout<<'J';
+
+cout<<'u';
+
+cout<<'i';
+
+cout<<'c';
+
+cout<<'e';
+
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"\nChose the type of the product:\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"KGRAM\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"LITER #\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"PIESE\nBAG";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+cout<<"\nEnter the amount of the product:\n";
+cout<<1;
+Sleep(T);
+cout<<"\nEnter the time creation of the product:\n";
+cout<<1;
+Sleep(T);
+cout<<2;
+Sleep(T);
+cout<<'.';
+Sleep(T);
+cout<<1;
+Sleep(T);
+cout<<5;
+Sleep(T);
+cout<<"\nEnter the day creation :            As in example '25.01.1997'\n";
+Sleep(T);
+cout<<25;
+Sleep(T);
+cout<<'.';
+Sleep(T);
+cout<<0<<1;
+Sleep(T);
+cout<<'.'<<2015;
+Sleep(T);
+cout<<"\nEnter the expired time\n";
+Sleep(T);
+cout<<300;
+cout<<"\nYour new product:\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),6);
+cout<<"ID          NAME    TYPE        AMOUNT      TIME        DATA               EXP. DATE\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),3);
+cout<<"029867      Juice   LITR        1.0         12.15       25.01 2015         300\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"SAVE\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"DONT SAVE";
+Sleep(T*2.2);
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"Add more products\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"Go to main menu";
+Sleep(2*T);
+system("cls");
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),12);
+cout<<"Add more products\n";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),74);
+cout<<"Go to main menu";
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),15);
+Sleep(2*T);
+
+
+
+}
+#endif // DEMONSTRTION_H_INCLUDED
